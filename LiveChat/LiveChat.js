a61abb056af9d59c737a9ce219822ea30f0d4d0e
Ben Nam <ben.nam@mavrix.es>
2018-04-13 09:54:35 +0200

    Removed location.protocol check

--- LiveChat/LiveChat.js
+++ LiveChat/LiveChat.js
@@ -30,7 +30,7 @@ export default class LiveChat extends React.Component {
       const lc = document.createElement('script');
       lc.type = 'text/javascript';
       lc.async = true;
-      lc.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+      lc.src = '//cdn.livechatinc.com/tracking.js';
       const s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(lc, s);
       lc.addEventListener('load', this.chatLoaded.bind(this));
