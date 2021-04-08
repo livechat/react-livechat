f8786d096865305f9cdd25b2553c6743fd257c1e
Daniel <daniel.shepard@incsub.com>
2021-04-08 14:30:42 -0700

    Use Livechat API 2.0 and new methods now.

--- LiveChat/LiveChat.js
+++ LiveChat/LiveChat.js
@@ -2,121 +2,187 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class LiveChat extends React.Component {
-  componentDidMount() {
-    this.loadLiveChatApi.bind(this)();
-  }
+	componentDidMount() {
+		this.loadLiveChatApi.bind( this )();
+	}
 
-  chatLoaded() {
-    if (window.LC_API) {
-      this.setCallbacks.bind(this)();
-      if (typeof this.props.onChatLoaded === 'function') {
-        this.props.onChatLoaded(window.LC_API);
-      }
-    }
-  }
+	chatLoaded() {
+		if ( window.LiveChatWidget ) {
+			this.setCallbacks.bind( this )();
+			if ( typeof this.props.onChatLoaded === 'function' ) {
+				this.props.onChatLoaded( window.LiveChatWidget );
+			}
+		}
+	}
 
-  chatNotLoaded() {
-    if (typeof this.props.onErrorLoading === 'function') {
-      this.props.onErrorLoading();
-    }
-  }
+	chatNotLoaded() {
+		if ( typeof this.props.onErrorLoading === 'function' ) {
+			this.props.onErrorLoading();
+		}
+	}
 
-  loadLiveChatApi() {
-    if (!window.LC_API) {
-      window.__lc = window.__lc || {};
-      window.__lc.license = this.props.license;
-      window.__lc.group = this.props.group;
-      window.__lc.chat_between_groups = this.props.chatBetweenGroups;
-      window.__lc.params = this.props.params;
-      window.__lc.visitor = this.props.visitor;
-      const lc = document.createElement('script');
-      lc.type = 'text/javascript';
-      lc.async = true;
-      lc.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
-      const s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(lc, s);
-      lc.addEventListener('load', this.chatLoaded.bind(this));
-      lc.addEventListener('error', this.chatNotLoaded.bind(this));
-    }
-  }
+	loadLiveChatApi() {
+		if ( ! window.LiveChatWidget ) {
+			window.__lc = window.__lc || {};
+			window.__lc.license = this.props.license;
+			window.__lc.group = this.props.group;
+			window.__lc.skill = this.props.group;
+			window.__lc.chat_between_groups = this.props.chatBetweenGroups;
+			window.__lc.params = this.props.params;
+			window.__lc.visitor = this.props.visitor;
+			const lc = document.createElement( 'script' );
+			lc.type = 'text/javascript';
+			lc.async = true;
+			lc.src =
+				( 'https:' === document.location.protocol
+					? 'https://'
+					: 'http://' ) + 'cdn.livechatinc.com/tracking.js';
+			const s = document.getElementsByTagName( 'script' )[ 0 ];
+			s.parentNode.insertBefore( lc, s );
+			this.setLiveChatWidget();
+			lc.addEventListener( 'load', this.chatLoaded.bind( this ) );
+			lc.addEventListener( 'error', this.chatNotLoaded.bind( this ) );
+		}
+	}
 
-  render() {
-    return null;
-  }
+	setLiveChatWidget() {
+		const c = [].slice;
+		function i( n ) {
+			return e._h ? e._h.apply( null, n ) : e._q.push( n );
+		}
+		const e = {
+			_q: [],
+			_h: null,
+			_v: '2.0',
+			on() {
+				i( [ 'on', c.call( arguments ) ] );
+			},
+			once() {
+				i( [ 'once', c.call( arguments ) ] );
+			},
+			off() {
+				i( [ 'off', c.call( arguments ) ] );
+			},
+			get() {
+				if ( ! e._h )
+					throw new Error(
+						"[LiveChatWidget] You can't use getters before load."
+					);
+				return i( [ 'get', c.call( arguments ) ] );
+			},
+			call() {
+				i( [ 'call', c.call( arguments ) ] );
+			},
+		};
+		return ( window.LiveChatWidget = window.LiveChatWidget || e );
+	}
 
-  setCallbacks() {
-    if (typeof this.props.onBeforeLoad === 'function')
-      window.LC_API.on_before_load = this.props.onBeforeLoad.bind(this);
+	render() {
+		return null;
+	}
 
-    if (typeof this.props.onAfterLoad === 'function')
-      window.LC_API.on_after_load = this.props.onAfterLoad.bind(this);
+	setCallbacks() {
+		if ( typeof this.props.onReady === 'function' ) {
+			window.LiveChatWidget.on(
+				'ready',
+				this.props.onReady.bind( this )
+			);
+		}
 
-    if (typeof this.props.onChatWindowOpened === 'function')
-      window.LC_API.on_chat_window_opened = this.props.onChatWindowOpened.bind(this);
+		if ( typeof this.props.onVisibilityChanged === 'function' ) {
+			window.LiveChatWidget.on(
+				'visibility_changed',
+				this.props.onVisibilityChanged.bind( this )
+			);
+		}
 
-    if (typeof this.props.onChatWindowMinimized === 'function')
-      window.LC_API.on_chat_window_minimized = this.props.onChatWindowMinimized.bind(this);
+		if ( typeof this.props.onCustomerStatusChanged === 'function' ) {
+			window.LiveChatWidget.on(
+				'customer_status_changed',
+				this.props.onCustomerStatusChanged.bind( this )
+			);
+		}
 
-    if (typeof this.props.onChatWindowHidden === 'function')
-      window.LC_API.on_chat_window_hidden = this.props.onChatWindowHidden.bind(this);
+		if ( typeof this.props.onChatWindowHidden === 'function' ) {
+			window.LiveChatWidget.on(
+				'visibility_changed',
+				this.props.onChatWindowHidden.bind( this )
+			);
+		}
 
-    if (typeof this.props.onChatStateChanged === 'function')
-      window.LC_API.on_chat_state_changed = this.props.onChatStateChanged.bind(this);
+		if ( typeof this.props.onNewEvent === 'function' ) {
+			window.LiveChatWidget.on(
+				'new_event',
+				this.props.onNewEvent.bind( this )
+			);
+		}
 
-    if (typeof this.props.onChatStarted === 'function')
-      window.LC_API.on_chat_started = this.props.onChatStarted.bind(this);
+		if ( typeof this.props.onFormSubmitted === 'function' ) {
+			window.LiveChatWidget.on(
+				'form_submitted',
+				this.props.onFormSubmitted.bind( this )
+			);
+		}
 
-    if (typeof this.props.onChatEnded === 'function')
-      window.LC_API.on_chat_ended = this.props.onChatEnded.bind(this);
+		if ( typeof this.props.onRatingSubmitted === 'function' ) {
+			window.LiveChatWidget.on(
+				'rating_submitted',
+				this.props.onRatingSubmitted.bind( this )
+			);
+		}
 
-    if (typeof this.props.onMessage === 'function')
-      window.LC_API.on_message = this.props.onMessage.bind(this);
+		if ( typeof this.props.onGreetingDisplayed === 'function' ) {
+			window.LiveChatWidget.on(
+				'greeting_displayed',
+				this.props.onGreetingDisplayed.bind( this )
+			);
+		}
 
-    if (typeof this.props.onTicketCreated === 'function')
-      window.LC_API.on_ticket_created = this.props.onTicketCreated.bind(this);
+		if ( typeof this.props.onGreetingHidden === 'function' ) {
+			window.LiveChatWidget.on(
+				'greeting_hidden',
+				this.props.onGreetingHidden.bind( this )
+			);
+		}
 
-    if (typeof this.props.onPrechatSurveySubmitted === 'function')
-      window.LC_API.on_prechat_survey_submitted = this.props.onPrechatSurveySubmitted.bind(this);
-
-    if (typeof this.props.onRatingSubmitted === 'function')
-      window.LC_API.on_rating_submitted = this.props.onRatingSubmitted.bind(this);
-
-    if (typeof this.props.onRatingCommentSubmitted === 'function')
-      window.LC_API.on_rating_comment_submitted = this.props.onRatingCommentSubmitted.bind(this);
-  }
+		if ( typeof this.props.onRichMessageButtonClicked === 'function' ) {
+			window.LiveChatWidget.on(
+				'rich_message_button_clicked',
+				this.props.onRichMessageButtonClicked.bind( this )
+			);
+		}
+	}
 }
 
 LiveChat.propTypes = {
-  // important
-  license: PropTypes.number.isRequired,
-  group: PropTypes.number,
-  onChatLoaded: PropTypes.func,
-  // less important
-  params: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    value: PropTypes.any.isRequired
-  })),
-  visitor: PropTypes.shape({
-      name: PropTypes.string,
-      email: PropTypes.string
-  }),
-  chatBetweenGroups: PropTypes.bool,
-  onBeforeLoad: PropTypes.func,
-  onAfterLoad: PropTypes.func,
-  onChatWindowOpened: PropTypes.func,
-  onChatWindowMinimized: PropTypes.func,
-  onChatWindowHidden: PropTypes.func,
-  onChatStateChanged: PropTypes.func,
-  onChatStarted: PropTypes.func,
-  onChatEnded: PropTypes.func,
-  onMessage: PropTypes.func,
-  onTicketCreated: PropTypes.func,
-  onPrechatSurveySubmitted: PropTypes.func,
-  onPostchatSurveySubmitted: PropTypes.func,
-  onRatingSubmitted: PropTypes.func,
-  onRatingCommentSubmitted: PropTypes.func,
+	// important
+	license: PropTypes.number.isRequired,
+	group: PropTypes.number,
+	onChatLoaded: PropTypes.func,
+	// less important
+	params: PropTypes.arrayOf(
+		PropTypes.shape( {
+			name: PropTypes.string.isRequired,
+			value: PropTypes.any.isRequired,
+		} )
+	),
+	visitor: PropTypes.shape( {
+		name: PropTypes.string,
+		email: PropTypes.string,
+	} ),
+	chatBetweenGroups: PropTypes.bool,
+	onReady: PropTypes.func,
+	onAvailabilityChanged: PropTypes.func,
+	onVisibilityChanged: PropTypes.func,
+	onCustomerStatusChanged: PropTypes.func,
+	onNewEvent: PropTypes.func,
+	onFormSubmitted: PropTypes.func,
+	onRatingSubmitted: PropTypes.func,
+	onGreetingDisplayed: PropTypes.func,
+	onGreetingHidden: PropTypes.func,
+	onRichMessageButtonClicked: PropTypes.func,
 };
 
 LiveChat.defaultProps = {
-  group: 0,
+	group: 0,
 };
